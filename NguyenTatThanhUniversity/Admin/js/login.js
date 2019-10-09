77664d26fdbc98940d073042330df8da2c12a828
= <=>
2019-10-09 17:34:01 +0700

    loan tai admin

--- NguyenTatThanhUniversity/Admin/js/login.js
+++ NguyenTatThanhUniversity/Admin/js/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#login').click(function() {
-        if ($('#name').val()=='HueLoan' && $('#pass').val()=='123456789') {
+        if ($('#name').val()=='loannt72@wru.vn' && $('#pass').val()=='loannguyen') {
             window.location.href='Quantrihethong.html';
         }else{
           $( ".notification" ).show();
