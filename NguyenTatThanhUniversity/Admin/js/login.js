fba2c3f9594f0cb0e8656e029348d1f54654b173
= <=>
2019-10-13 11:51:32 +0700

    Loan sua User va Admin

--- NguyenTatThanhUniversity/Admin/js/login.js
+++ NguyenTatThanhUniversity/Admin/js/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#login').click(function() {
-        if ($('#name').val()=='loannt72@wru.vn' && $('#pass').val()=='loannguyen') {
+        if ($('#name').val()=='loannt72@wru.vn ' && $('#pass').val()=='loannguyen') {
             window.location.href='Quantrihethong.html';
         }else{
           $( ".notification" ).show();
